9f8ae64b192305f433720b50eee367e03b6dfb2e
jessy hazart <jessy.hazart@gmail.com>
2022-12-05 16:07:34 +0100

    cleaned useless code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,7 @@
-import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
