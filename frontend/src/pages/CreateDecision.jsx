56f8ede01b57d96a8394370460272b35c502e0ab
Mounir-mk <mounir.mekoui@gmail.com>
2022-12-06 15:19:14 +0100

    added sample form

--- frontend/src/pages/CreateDecision.jsx
+++ frontend/src/pages/CreateDecision.jsx
@@ -1,5 +1,15 @@
 function CreateDecision() {
-  return <h1>Créer une nouvelle décision</h1>;
+  return (
+    <main>
+      <h1>Create Decision</h1>
+      <form action="">
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" />
+        <label htmlFor="description">Description</label>
+        <input type="text" id="description" />
+      </form>
+    </main>
+  );
 }
 
 export default CreateDecision;
