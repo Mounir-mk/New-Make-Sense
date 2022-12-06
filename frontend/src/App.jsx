a6db9744cb80abf45ddf779c28a133ecd7bcba4f
jessy hazart <jessy.hazart@gmail.com>
2022-12-06 10:46:31 +0100

    first-page and created a route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import CreateDecision from "./pages/CreateDecision";
 import "./App.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<CreateDecision />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
