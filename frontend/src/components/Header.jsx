723b71e5cffa8d64c21cc3d4eae837e0601af280
Mounir-mk <mounir.mekoui@gmail.com>
2022-12-06 11:56:05 +0100

    Add header content with some svg in assets folder

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,47 @@
+import React from "react";
+import logo from "../assets/logo_makesense.png";
+import homeIcon from "../assets/home.svg";
+import calendarIcon from "../assets/calendar.svg";
+import smileIcon from "../assets/smile.svg";
+import menuIcon from "../assets/menu.svg";
+import userIcon from "../assets/sampleprofile.png";
+
 function Header() {
-  return <h1>Logo</h1>;
+  return (
+    <header className="w-screen flex items-center justify-around border-solid border-2 h-[10%]">
+      <img src={logo} alt="MakeSense" className="max-h-4 md:max-h-8 w-auto" />
+      <nav>
+        <ul className="flex gap-4">
+          <li className="flex flex-col hover:underline justify-center">
+            <img src={homeIcon} alt="Home" className="max-h-4 w-auto" />
+            <span className="hidden md:inline font-bold text-xs">
+              Parcourir les décisions
+            </span>
+          </li>
+          <li className="flex flex-col hover:underline justify-center">
+            <img src={calendarIcon} alt="Calendar" className="max-h-4 w-auto" />
+            <span className="hidden md:inline font-bold text-xs">
+              Mes décisions
+            </span>
+          </li>
+          <li className="flex flex-col hover:underline justify-center">
+            <img src={smileIcon} alt="Smile" className="max-h-4 w-auto" />
+            <span className="hidden md:inline font-bold text-xs">
+              Notifications
+            </span>
+          </li>
+          <li className="w-20 h-8 rounded-full bg-slate-400 flex justify-around items-center">
+            <img src={menuIcon} alt="Menu" className="max-h-4 w-auto" />
+            <img
+              src={userIcon}
+              alt="User"
+              className="h-8 w-8 rounded-full object-cover"
+            />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
+
 export default Header;
