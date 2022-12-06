410eb20099079620b7c53ed022aa5f4f8ba04926
jessy hazart <jessy.hazart@gmail.com>
2022-12-06 11:26:51 +0100

    added header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import CreateDecision from "./pages/CreateDecision";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<CreateDecision />} />
       </Routes>
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,4 @@
+function Header() {
+  return <h1>Logo</h1>;
+}
+export default Header;
