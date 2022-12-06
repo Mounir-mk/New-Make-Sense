5b2f25cc71c6ae43d44d72aa227c1f3cf2e201d9
Mounir-mk <mounir.mekoui@gmail.com>
2022-12-06 10:45:10 +0100

    add tailwind to the project + import Raleway font

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <h1 className="font-bold text-center text-3xl underline">React App</h1>
+    </div>
+  );
 }
 
 export default App;
