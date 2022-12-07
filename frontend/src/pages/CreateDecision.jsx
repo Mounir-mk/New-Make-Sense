b015bda36748eb418508e9a8e3852045e8621200
Mounir-mk <mounir.mekoui@gmail.com>
2022-12-07 16:13:00 +0100

    creation of 2 components inside the page, the first one corresponds to the decision description form, the second one, reusable, corresponds to the impact, benefit and risk text areas. All input data are stored in the createDecision object.

--- frontend/src/pages/CreateDecision.jsx
+++ frontend/src/pages/CreateDecision.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import rightchevron from "../assets/chevron-right.svg";
-import plusIcon from "../assets/plus.svg";
+import DescriptionDecisionForm from "../components/DescriptionDecisionForm";
+import InputDecisionForm from "../components/InputDecisionForm";
 
 function CreateDecision() {
   const [inputImpacted, setInputImpacted] = useState("");
@@ -84,153 +85,41 @@ function CreateDecision() {
           </li>
         </ul>
         <div className="md:flex md:justify-evenly w-full gap-4">
-          <section className="flex gap-2 w-full justify-evenly">
-            <div className="flex flex-col gap-1 items-center w-1/2">
-              <h2 className="font-bold">Impactés</h2>
-              <div className="relative w-full">
-                <input
-                  type="text"
-                  placeholder="@Impactés"
-                  name="impacted"
-                  className="border-2 border-slate-500 rounded-xl px-2 md:px-4 py-1 md:py-2 w-full"
-                  value={inputImpacted}
-                  onChange={(event) => setInputImpacted(event.target.value)}
-                />
-                <button
-                  className="absolute right-0 h-full"
-                  type="button"
-                  onClick={() => {
-                    setCreateDecision({
-                      ...createDecision,
-                      impacted: [...createDecision.impacted, inputImpacted],
-                    });
-                    setInputImpacted("");
-                  }}
-                >
-                  <img src={plusIcon} alt="Plus" className="max-h-6 w-auto" />
-                </button>
-              </div>
-              <ul className="flex gap-1 flex-wrap self-start md:my-16 w-full">
-                {createDecision.impacted.map((impactElement) => (
-                  <li
-                    className="text-[#0C3944] text-xl rounded-full text-center px-4 py-2 font-bold border border-black"
-                    key={impactElement}
-                  >
-                    {`#${
-                      impactElement.charAt(0).toUpperCase() +
-                      impactElement.slice(1)
-                    }`}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="flex flex-col gap-1 items-center w-1/2">
-              <h2 className="font-bold">Experts</h2>
-              <div className="relative w-full">
-                <input
-                  type="text"
-                  placeholder="@Experts"
-                  name="expert"
-                  className="border-2 border-slate-500 rounded-xl px-2 md:px-4 py-1 md:py-2 w-full"
-                  value={inputExpert}
-                  onChange={(event) => setInputExpert(event.target.value)}
-                />
-                <button
-                  className="absolute right-0 h-full"
-                  type="button"
-                  onClick={() => {
-                    setCreateDecision({
-                      ...createDecision,
-                      experts: [...createDecision.experts, inputExpert],
-                    });
-                    setInputExpert("");
-                  }}
-                >
-                  <img src={plusIcon} alt="Plus" className="max-h-6 w-auto" />
-                </button>
-              </div>
-              <ul className="flex gap-1 flex-wrap self-start md:my-16 w-full">
-                {createDecision.experts.map((expertElement) => (
-                  <li
-                    className="text-[#0C3944] text-xl rounded-full text-center px-4 py-2 font-bold border border-black"
-                    key={expertElement}
-                  >
-                    {`#${
-                      expertElement.charAt(0).toUpperCase() +
-                      expertElement.slice(1)
-                    }`}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </section>
-          <form className="flex flex-col gap-2 md:gap-4">
-            <div className="flex gap-3">
-              <div className="flex flex-col gap-1">
-                <label htmlFor="decision">
-                  <h2 className="font-bold text-center">
-                    Titre de la décision
-                  </h2>
-                </label>
-                <input
-                  type="text"
-                  name="decision"
-                  id="decision"
-                  className="border-2 border-slate-500 rounded-xl px-2 md:px-4 py-1 md:py-2"
-                  value={createDecision.title}
-                  onChange={(event) =>
-                    setCreateDecision((old) => ({
-                      ...old,
-                      title: event.target.value,
-                    }))
-                  }
-                />
-              </div>
-              <div className="flex flex-col gap-1">
-                <label htmlFor="deadline">
-                  <h2 className="font-bold text-center">Date limite</h2>
-                </label>
-                <input
-                  type="date"
-                  name="deadline"
-                  id="deadline"
-                  className="border-2 border-slate-500 rounded-xl px-2 md:px-4 py-1 md:py-2"
-                  value={createDecision.date}
-                  onChange={(event) => {
-                    setCreateDecision((old) => ({
-                      ...old,
-                      date: event.target.value,
-                    }));
-                  }}
-                />
-              </div>
-            </div>
-            <label htmlFor="description">
-              <h2 className="font-bold text-center">Description</h2>
-            </label>
-            <textarea
-              name="description"
-              id="description"
-              className="border-2 border-slate-500 rounded-xl px-2 md:px-4 py-1 md:py-2 h-56 md:h-96 w-full"
-              value={createDecision.description}
-              onChange={(event) =>
-                setCreateDecision((old) => ({
-                  ...old,
-                  description: event.target.value,
-                }))
-              }
+          {step === 1 && (
+            <DescriptionDecisionForm
+              inputImpacted={inputImpacted}
+              setInputImpacted={setInputImpacted}
+              inputExpert={inputExpert}
+              setInputExpert={setInputExpert}
+              createDecision={createDecision}
+              setCreateDecision={setCreateDecision}
+              setStep={setStep}
             />
-            <button
-              type="submit"
-              className="font-bold text-sm rounded-full px-3 py-1 md:text-xl whitespace-nowrap bg-[#9B084F] text-white"
-              onClick={(e) => {
-                e.preventDefault();
-                setStep((old) => old + 1);
-              }}
-            >
-              Suivant
-            </button>
-          </form>
+          )}
+          {step === 2 && (
+            <InputDecisionForm
+              createDecision={createDecision}
+              setCreateDecision={setCreateDecision}
+              setStep={setStep}
+              stepName="impacts"
+            />
+          )}
+          {step === 3 && (
+            <InputDecisionForm
+              createDecision={createDecision}
+              setCreateDecision={setCreateDecision}
+              setStep={setStep}
+              stepName="advantages"
+            />
+          )}
+          {step === 4 && (
+            <InputDecisionForm
+              createDecision={createDecision}
+              setCreateDecision={setCreateDecision}
+              setStep={setStep}
+              stepName="risks"
+            />
+          )}
         </div>
       </div>
     </main>
