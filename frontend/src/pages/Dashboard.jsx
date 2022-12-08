b2e1ca6b09d565b4b907d4b3aab927c161c158f9
jessy hazart <jessy.hazart@gmail.com>
2022-12-08 16:32:02 +0100

    structured dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,24 @@
 function Dashboard() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <section>
+      <h1>Décisions en cours</h1>
+      <div className="running-decision-dashboard">
+        <div className="decision-card">
+          <h3>Lorem ipsum</h3>
+          <div className="decision-maker-info">
+            <img src="#" alt="profil" />
+            <p>par John Doe</p>
+          </div>
+        </div>
+        <div className="decision-card">
+          <h3>Lorem ipsum</h3>
+          <div className="decision-maker-info">
+            <img src="#" alt="profil" />
+            <p>par John Doe</p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
