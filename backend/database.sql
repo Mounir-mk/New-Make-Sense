549fa14e9232c5e05ed2be8927661f4f73f9b279
jessy hazart <jessy.hazart@gmail.com>
2022-12-08 14:55:48 +0100

    added foreign key to decision

--- backend/database.sql
+++ backend/database.sql
@@ -15,7 +15,9 @@ create table decision (
   content varchar(250) NOT NULL,
   impact varchar(250) NOT NULL,
   risk varchar(250) NOT NULL,
-  advantage varchar(250) NOT NULL
+  advantage varchar(250) NOT NULL,
+  user_id int(11) unsigned NOT NULL,
+  constraint decision_user foreign key (user_id) references user(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO decision (title, deadline, content, impact, risk, advantage) VALUES ('Titre de la décision','2022-12-07','Lorem ipsum content','Lorem ipsum impact','Lorem ipsum risk','Lorem ipsum advantage');
+INSERT INTO decision (title, deadline, content, impact, risk, advantage, user_id) VALUES ('Titre de la décision','2022-12-07','Lorem ipsum content','Lorem ipsum impact','Lorem ipsum risk','Lorem ipsum advantage', 1), ('Titre de la décision 2','2022-12-08','Lorem ipsum content2','Lorem ipsum impact2','Lorem ipsum risk2','Lorem ipsum advantage2', 1);
