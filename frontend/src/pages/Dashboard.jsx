2f8de278225c20cc1380eed5052dafc7b509b5b1
Mounir-mk <mounir.mekoui@gmail.com>
2022-12-09 11:32:39 +0100

    styling dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,15 +2,19 @@ import DashboardCard from "../components/DecisionCard";
 
 function Dashboard() {
   return (
-    <section>
-      <h1>Décisions en cours</h1>
-      <div className="running-decision-dashboard">
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-      </div>
-    </section>
+    <main className="flex justify-center">
+      <section id="current-decision" className="w-2/3 mt-24 mb-20">
+        <h1 className="font-bold text-3xl text-blue-dianne pb-12 border-b-2">
+          Décisions en cours
+        </h1>
+        <div className="flex flex-col md:flex-row gap-4 flex-wrap">
+          <DashboardCard
+            decisionTitle="Déménager hors de paris à printemps 2023 (fin Avril) et tester Milan"
+            author="Jane Doe"
+          />
+        </div>
+      </section>
+    </main>
   );
 }
 
