912821675d2bca959660aab1c85743d4004ca0a5
Mounir-mk <mounir.mekoui@gmail.com>
2022-12-09 18:37:46 +0100

    store data when click on 'voir' button

--- frontend/src/components/DecisionCard.jsx
+++ frontend/src/components/DecisionCard.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import avatar from "../assets/profile_pic_default.svg";
 
-function DashboardCard({ decisionTitle, author }) {
+function DashboardCard({ decisionTitle, author, id }) {
+  const [decicionData, setDecisionData] = useState({});
+  // fectch data of the decision when the user click on the button "Voir
+  // la décision"
+  const handleClick = () => {
+    fetch(`http://localhost:5000/decisions/${id}`)
+      .then((res) => res.json())
+      .then((data) => setDecisionData(data));
+  };
+
+  console.warn(decicionData);
+
   return (
     <section className="bg-white rounded-xl p-4 flex flex-col justify-evenly shadow-2xl my-4 w-72 h-56">
       <h1 className="font-bold text-blue-dianne text-lg">{decisionTitle}</h1>
@@ -16,6 +28,7 @@ function DashboardCard({ decisionTitle, author }) {
           <button
             type="button"
             className="bg-[#9B084F] text-white rounded-md px-4 py-2 ml-auto"
+            onClick={() => handleClick()}
           >
             Voir
           </button>
@@ -28,6 +41,7 @@ function DashboardCard({ decisionTitle, author }) {
 DashboardCard.propTypes = {
   decisionTitle: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default DashboardCard;
