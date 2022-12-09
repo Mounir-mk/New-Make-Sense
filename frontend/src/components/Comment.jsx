3f465a86320a25b68b3dcf579e4ea4f64cd12b19
Mounir-mk <mounir.mekoui@gmail.com>
2022-12-09 10:57:07 +0100

    adding a state to store comment and displaying new ones, restyling a bit to be more user friendly

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Comment({ icon }) {
+function Comment({ icon, comment }) {
   return (
-    <div className="flex flex-col gap-3 bg-red-50 rounded-lg border-2 md:w-2/3 border-r-2 my-4">
+    <div className="flex flex-col gap-3 bg-red-50 rounded-lg border-2 md:w-2/3 border-r-2 my-4 mx-4 md:mx-0">
       <div className="flex items-center gap-2 p-2">
         <img src={icon} alt="cat" className="w-12 h-12 rounded-full" />
         <div className="flex flex-col">
@@ -15,18 +15,14 @@ function Comment({ icon }) {
         id="separating-bar"
         className="border-b-2 border-gray-300 w-1/2 flex justify-center items-center"
       />
-      <p className="text-sm p-2">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt,
-        nisl eget ultricies tincidunt, nisl nisl aliquam nisl, eu aliquam nisl
-        nisl sit amet nisl. Sed tincidunt, nisl eget ultricies tincidunt, nisl
-        nisl aliquam nisl, eu aliquam nisl nisl sit amet nisl.
-      </p>
+      <p className="text-sm p-2">{comment}</p>
     </div>
   );
 }
 
 Comment.propTypes = {
   icon: PropTypes.string.isRequired,
+  comment: PropTypes.string.isRequired,
 };
 
 export default Comment;
