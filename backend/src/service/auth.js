05b3091a21840cebe8b1a4799beee3278daa6987
Mounir <85800425+Mounir-mk@users.noreply.github.com>
2022-12-14 17:34:08 +0100

    adding a hashPassword middleware and putting it on put and post route

--- backend/src/service/auth.js
+++ backend/src/service/auth.js
@@ -1,24 +1,25 @@
 const argon2 = require("argon2");
 
 const hashPassword = (req, res, next) => {
-    const hashOptions = {
-        type: argon2.argon2id,
-        memoryCost: 2 ** 16,
-        parallelism: 1,
-        timeCost: 5,
-    };
-    argon2
-        .hash(req.body.password, hashOptions)
-        .then((hashPassword) => {
-            req.body.password = hashPassword;
-            delete req.body.password;
-            next();
-        })
-        .catch((err) => {
-            res.status(500).json({ message: "Internal server error" });
-        });
+  const hashOptions = {
+    type: argon2.argon2id,
+    memoryCost: 2 ** 16,
+    parallelism: 1,
+    timeCost: 5,
+  };
+  argon2
+    .hash(req.body.password, hashOptions)
+    .then((hashedPassword) => {
+      req.body.password = hashedPassword;
+      delete req.body.password;
+      next();
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
 };
 
 module.exports = {
-    hashPassword,
+  hashPassword,
 };
