2718e763b6db0230937fafa428f915a4400ace13
jessy hazart <jessy.hazart@gmail.com>
2022-12-19 15:50:43 +0100

    added route to insert concerned users in table concerned

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,6 +8,7 @@ router.get("/decisions", decisionControllers.browse);
 router.get("/decisions/:id", decisionControllers.read);
 router.put("/decisions/:id", decisionControllers.edit);
 router.post("/decisions", decisionControllers.add);
+router.post("/decisions/:id/users", decisionControllers.addConcerned);
 router.delete("/decisions/:id", decisionControllers.destroy);
 
 const userControllers = require("./controllers/userControllers");
@@ -18,13 +19,5 @@ router.get("/users/:id", userControllers.read);
 router.put("/users/:id", hashPassword, userControllers.edit);
 router.post("/users", hashPassword, userControllers.add);
 router.delete("/users/:id", userControllers.destroy);
-
-const concernedControllers = require("./controllers/concernedControllers");
-
-router.get("/concerned", concernedControllers.browse);
-router.get("/concerned/:id", concernedControllers.read);
-router.put("/concerned/:id", hashPassword, concernedControllers.edit);
-router.post("/concerned", hashPassword, concernedControllers.add);
-router.delete("/concerned/:id", concernedControllers.destroy);
 
 module.exports = router;
