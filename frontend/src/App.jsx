4957753ce0a7d06f92248a66a6888d0ad8aec957
Mounir <85800425+Mounir-mk@users.noreply.github.com>
2022-12-20 13:25:19 +0100

    added a useEffect which redirects us to /login if we are authenticated, (excluding register page)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { Route, Routes } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { AuthContext } from "./_services/AuthContext";
 import CreateDecision from "./pages/CreateDecision";
 import Decision from "./pages/Decision";
@@ -10,21 +10,30 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 
 function App() {
+  const navigate = useNavigate();
   const { auth } = useContext(AuthContext);
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      if (
+        window.location.pathname !== "/register" &&
+        window.location.pathname !== "/login"
+      ) {
+        navigate("/login");
+      }
+    }
+  }, [auth.isAuthenticated, navigate]);
   return (
     <div className="App">
       {auth.isAuthenticated && <Header />}
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        {auth.isAuthenticated ? (
+        {auth.isAuthenticated && (
           <>
             <Route path="/decisions/create" element={<CreateDecision />} />
             <Route path="/decision/:id" element={<Decision />} />
             <Route path="/" element={<Dashboard />} />
           </>
-        ) : (
-          <Route path="/" element={<Login />} />
         )}
       </Routes>
     </div>
