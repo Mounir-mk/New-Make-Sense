91b2143a15fe5d5c16ab2715a1a04b1a3639c61e
jessy hazart <jessy.hazart@gmail.com>
2023-01-30 17:08:54 +0100

    edited progress bar

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import { Progress } from "rsuite";
 import { getDate, convertToFr } from "../services/dateFunctions";
 
 function Timeline({ startDate, endDate }) {
@@ -9,101 +8,99 @@ function Timeline({ startDate, endDate }) {
   );
   return (
     <>
-      <div className="flex rotate-180 absolute h-[45%] my-14">
-        {durationPercentage <= 100 ? (
-          <Progress.Line
-            vertical
-            percent={durationPercentage}
-            strokeColor="#C1E94E"
-            showInfo={false}
-            status="active"
-          />
-        ) : (
-          <Progress.Line
-            vertical
-            percent="100"
-            strokeColor="#C1E94E"
-            showInfo={false}
-            status="success"
-          />
-        )}
-      </div>
-      <div id="timeline" className="flex flex-col">
-        <h1 className="font-bold text-base">Dates à retenir</h1>
-        <ol className="p-7">
-          <li>
-            <div className="flex flex-start items-center pt-2">
-              <p className="text-gray-500 text-sm">{convertToFr(startDate)}</p>
-            </div>
-            <div className="mt-0.5 ml-4 mb-6">
-              <h4 className="text-gray-800 font-semibold text-sm mb-1.5">
-                Décision débutée
-              </h4>
-            </div>
-          </li>
-          <li>
-            <div className="flex flex-start items-center pt-2">
-              <p className="text-gray-500 text-sm">
-                {startDate !== "" &&
-                  convertToFr(
-                    new Date(
-                      publishDate.getTime() + statusDuration
-                    ).toISOString()
-                  )}
-              </p>
-            </div>
-            <div className="mt-0.5 ml-4 mb-6">
-              <h4 className="text-gray-800 font-semibold text-sm mb-1.5">
-                Première décision prise
-              </h4>
-            </div>
-          </li>
-          <li>
-            <div className="flex flex-start items-center pt-2">
-              <p className="text-gray-500 text-sm">
-                {startDate !== "" &&
-                  convertToFr(
-                    new Date(
-                      publishDate.getTime() + statusDuration * 2
-                    ).toISOString()
-                  )}
-              </p>
-            </div>
-            <div className="mt-0.5 ml-4 mb-6">
-              <h4 className="text-gray-800 font-semibold text-sm mb-1.5">
-                Conflit sur la décision
-              </h4>
-            </div>
-          </li>
-          <li>
-            <div className="flex flex-start items-center pt-2">
-              <p className="text-gray-500 text-sm">
-                {startDate !== "" &&
-                  convertToFr(
-                    new Date(
-                      publishDate.getTime() + statusDuration * 3
-                    ).toISOString()
-                  )}
-              </p>
-            </div>
-            <div className="mt-0.5 ml-4 mb-6">
-              <h4 className="text-gray-800 font-semibold text-sm mb-1.5">
-                Décision définitive
-              </h4>
-            </div>
-          </li>
-          <li>
-            <div className="flex flex-start items-center pt-2">
-              <p className="text-gray-500 text-sm">{convertToFr(endDate)}</p>
-            </div>
-            <div className="mt-0.5 ml-4 pb-5">
-              <h4 className="text-gray-800 font-semibold text-sm mb-1.5">
-                Décision terminée
-              </h4>
-            </div>
-          </li>
-        </ol>
-      </div>
+      <h1 className="font-bold text-base">Dates à retenir</h1>
+      <section className="flex items-center h-[400px] md:h-auto">
+        <div className="bg-slate-200 w-3 rounded-xl h-[90%] mr-4">
+          {durationPercentage <= 100 ? (
+            <div
+              className="relative w-full rounded-xl bg-green-500 z-50"
+              style={{ height: `${durationPercentage}%` }}
+            />
+          ) : (
+            <div
+              className="relative w-full rounded-xl bg-green-500 z-50"
+              style={{ height: "100%" }}
+            />
+          )}
+        </div>
+        <div id="timeline">
+          <ol className="flex flex-col justify-between">
+            <li>
+              <div className="flex flex-start items-center pt-2">
+                <p className="text-gray-500 text-sm">
+                  {convertToFr(startDate)}
+                </p>
+              </div>
+              <div className="mt-0.5 ml-4 mb-6">
+                <h4 className="text-gray-800 font-semibold text-sm mb-1.5">
+                  Décision débutée
+                </h4>
+              </div>
+            </li>
+            <li>
+              <div className="flex flex-start items-center pt-2">
+                <p className="text-gray-500 text-sm">
+                  {startDate !== "" &&
+                    convertToFr(
+                      new Date(
+                        publishDate.getTime() + statusDuration
+                      ).toISOString()
+                    )}
+                </p>
+              </div>
+              <div className="mt-0.5 ml-4 mb-6">
+                <h4 className="text-gray-800 font-semibold text-sm mb-1.5">
+                  Deadline pour donner son avis
+                </h4>
+              </div>
+            </li>
+            <li>
+              <div className="flex flex-start items-center pt-2">
+                <p className="text-gray-500 text-sm">
+                  {startDate !== "" &&
+                    convertToFr(
+                      new Date(
+                        publishDate.getTime() + statusDuration * 2
+                      ).toISOString()
+                    )}
+                </p>
+              </div>
+              <div className="mt-0.5 ml-4 mb-6">
+                <h4 className="text-gray-800 font-semibold text-sm mb-1.5">
+                  Première décision prise
+                </h4>
+              </div>
+            </li>
+            <li>
+              <div className="flex flex-start items-center pt-2">
+                <p className="text-gray-500 text-sm">
+                  {startDate !== "" &&
+                    convertToFr(
+                      new Date(
+                        publishDate.getTime() + statusDuration * 3
+                      ).toISOString()
+                    )}
+                </p>
+              </div>
+              <div className="mt-0.5 ml-4 mb-6">
+                <h4 className="text-gray-800 font-semibold text-sm mb-1.5">
+                  Deadline pour rentrer en conflit
+                </h4>
+              </div>
+            </li>
+            <li>
+              <div className="flex flex-start items-center pt-2">
+                <p className="text-gray-500 text-sm">{convertToFr(endDate)}</p>
+              </div>
+              <div className="mt-0.5 ml-4 pb-5">
+                <h4 className="text-gray-800 font-semibold text-sm mb-1.5">
+                  Décision définitive
+                </h4>
+              </div>
+            </li>
+          </ol>
+        </div>
+      </section>
     </>
   );
 }
