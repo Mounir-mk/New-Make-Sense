ab97a8507a14822c3c33fabc190bfe38dd94117f
jessy hazart <jessy.hazart@gmail.com>
2023-01-31 15:28:03 +0100

    refactored more Timeline.jsx

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -7,65 +7,50 @@ function Timeline({ startDate, endDate }) {
     startDate,
     endDate
   );
+  const titles = [
+    "Décision débutée",
+    "Deadline pour donner son avis",
+    "Première décision prise",
+    "Deadline pour rentrer en conflit",
+    "Décision définitive",
+  ];
+
+  const dates = [
+    convertToFr(startDate),
+    startDate !== "" &&
+      convertToFr(
+        new Date(publishDate.getTime() + statusDuration).toISOString()
+      ),
+    startDate !== "" &&
+      convertToFr(
+        new Date(publishDate.getTime() + statusDuration * 2).toISOString()
+      ),
+    startDate !== "" &&
+      convertToFr(
+        new Date(publishDate.getTime() + statusDuration * 3).toISOString()
+      ),
+    convertToFr(endDate),
+  ];
   return (
     <>
       <h1 className="font-bold text-base">Dates à retenir</h1>
       <section className="flex items-center h-[400px] md:h-auto">
         <div className="bg-slate-200 w-3 rounded-xl h-[90%] mr-4">
-          {durationPercentage <= 100 ? (
-            <div
-              className="relative w-full rounded-xl bg-green-500 z-50"
-              style={{ height: `${durationPercentage}%` }}
-            />
-          ) : (
-            <div
-              className="relative w-full rounded-xl bg-green-500 z-50"
-              style={{ height: "100%" }}
-            />
-          )}
+          <div
+            className="relative w-full rounded-xl bg-green-500 z-50"
+            style={{
+              height: `${
+                durationPercentage <= 100 ? durationPercentage : 100
+              }%`,
+            }}
+          />
         </div>
 
         <div id="timeline">
           <ol className="flex flex-col justify-between">
-            <TimelineSection
-              title="Décision débutée"
-              date={convertToFr(startDate)}
-            />
-            <TimelineSection
-              title="Deadline pour donner son avis"
-              date={
-                startDate !== "" &&
-                convertToFr(
-                  new Date(publishDate.getTime() + statusDuration).toISOString()
-                )
-              }
-            />
-            <TimelineSection
-              title="Première décision prise"
-              date={
-                startDate !== "" &&
-                convertToFr(
-                  new Date(
-                    publishDate.getTime() + statusDuration * 2
-                  ).toISOString()
-                )
-              }
-            />
-            <TimelineSection
-              title="Deadline pour rentrer en conflit"
-              date={
-                startDate !== "" &&
-                convertToFr(
-                  new Date(
-                    publishDate.getTime() + statusDuration * 3
-                  ).toISOString()
-                )
-              }
-            />
-            <TimelineSection
-              title="Décision définitive"
-              date={convertToFr(endDate)}
-            />
+            {titles.map((title, index) => (
+              <TimelineSection key={title} title={title} date={dates[index]} />
+            ))}
           </ol>
         </div>
       </section>
