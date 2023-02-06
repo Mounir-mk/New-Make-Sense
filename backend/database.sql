ca603aeec518375953164c215a23bfe4509a4f44
ROBIN KOLASINSKI <robin.kolasinski@gmail.com>
2023-02-06 09:11:29 +0100

    changed user names

--- backend/database.sql
+++ backend/database.sql
@@ -9,9 +9,9 @@ create table user (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO user (firstname, lastname, image_url, role, email, hashed_password) VALUES 
-('John','Doe','visitor.png','visitor','visitor@mail.com','$argon2id$v=19$m=65536,t=5,p=1$Yx+5bYJ3zsf1RHN92/SlxQ$VmX8cR64b7G3q+AuxSmcDJplhwMiz+QpeKdpN6GGEwI'),
-('Jane','Doe','employee.png','employee','employee@mail.com','$argon2id$v=19$m=65536,t=5,p=1$Yx+5bYJ3zsf1RHN92/SlxQ$VmX8cR64b7G3q+AuxSmcDJplhwMiz+QpeKdpN6GGEwI'),
-('Jack','Doe','admin.png','admin','admin@mail.com','$argon2id$v=19$m=65536,t=5,p=1$Yx+5bYJ3zsf1RHN92/SlxQ$VmX8cR64b7G3q+AuxSmcDJplhwMiz+QpeKdpN6GGEwI');
+('David','Douillet','visitor.png','visitor','visitor@mail.com','$argon2id$v=19$m=65536,t=5,p=1$Yx+5bYJ3zsf1RHN92/SlxQ$VmX8cR64b7G3q+AuxSmcDJplhwMiz+QpeKdpN6GGEwI'),
+('Cécile','Smith','employee.png','employee','employee@mail.com','$argon2id$v=19$m=65536,t=5,p=1$Yx+5bYJ3zsf1RHN92/SlxQ$VmX8cR64b7G3q+AuxSmcDJplhwMiz+QpeKdpN6GGEwI'),
+('Jack','Black','admin.png','admin','admin@mail.com','$argon2id$v=19$m=65536,t=5,p=1$Yx+5bYJ3zsf1RHN92/SlxQ$VmX8cR64b7G3q+AuxSmcDJplhwMiz+QpeKdpN6GGEwI');
 
 create table decision (
   id int(11) unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
